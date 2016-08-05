b7c625f94c284ad1f7a3ef176ef9f56624dd217b
Grace Patuwo <grace.patuwo@gmail.com>
2016-08-05 11:34:14 -0700

    created session controller and session login form

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+    params[:user][:username],
+    params[:user][:password])
+    if @user
+      login!(@user)
+      redirect_to user_url(@user.id)
+    else
+      flash.now[:errors] = "Wrong credentials"
+      render :new
+    end
+  end
+
+  def destroy
+    @user.reset_session_token!
+    session[:session_token] = ""
+  end
+
+end
